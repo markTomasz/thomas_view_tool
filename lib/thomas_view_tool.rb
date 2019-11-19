51b64cc7c2377487bb26bf91a192326a64541a7f
Mark Thomas <mdvthomas@gmail.com>
2019-11-18 21:31:19 -0500

    Dev tool init commit

---
+++ lib/thomas_view_tool.rb
@@ -0,0 +1,7 @@
+require "thomas_view_tool/version"
+require "thomas_view_tool/renderer"
+
+module ThomasViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/thomas_view_tool/renderer.rb
@@ -0,0 +1,9 @@
+module ThomasViewTool
+  class Error < StandardError; end
+
+  class Renderer
+    def self.copyright(name, msg)
+      "&copy; #{Time.now.year} | <strong>#{name}</strong> #{msg}".html_safe
+    end
+  end
+end
